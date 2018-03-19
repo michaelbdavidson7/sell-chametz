93a502d0baf1fe679d62ae9ca8ec3d1c082acd40
Eleanor.Davidson <michaelbdavidson7@gmail.com>
2018-03-19 01:02:34 -0400

    Added address lookup template code

--- handler.js
+++ handler.js
@@ -1,16 +1,11 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+/**
+ * This is a simple file that aliases your skill handler to a root
+ * level file so that you don't have to tinker with Lambda handler paths.
+ * This will make the default lambda value of "index.handler" work.
+ */
 
-  callback(null, response);
+const MyAlexaSkill = require('./lambda/custom/index');
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
+exports.handler = MyAlexaSkill.handler;
